e21f0e9c2355af099b41b8f2cfc8298697a76cb7
Diego Vilariño <diego.vilarino@gmail.com>
2017-10-14 23:42:15 +0200

    remove unused code

--- main.js
+++ main.js
@@ -1,21 +1,8 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
-
-const path = require('path')
-const url = require('url')
+const { app, Tray, Menu } = require('electron')
 
 /* =============================================================================
 MU - MAC UTILITIES APP
 ============================================================================= */
-
-/**
- * Application vars
- */
-const Tray = electron.Tray;
-const Menu = electron.Menu;
 
 const shell = require('shelljs');
 shell.config.execPath = '/usr/local/bin/node';
