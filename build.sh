7525c6ef929e432b169a6a730e70b934ba1573e8
Diego Vilariño <diego.vilarino@gmail.com>
2017-10-14 23:43:14 +0200

    update build script, create dmg, remove zip

--- build.sh
+++ build.sh
@@ -3,4 +3,9 @@
 #
 # sh build.sh
 #
-electron-packager ./ --platform=darwin --arch=x64 --overwrite --version=0.0.1 --icon=./mu.icns
+electron-packager ./ --platform=darwin --arch=x64 --overwrite --version=0.0.1 --icon=./mu.icns &&
+mkdir mu-darwin-x64/dmg &&
+ln -s /Applications mu-darwin-x64/dmg/Applications &&
+mv mu-darwin-x64/mu.app mu-darwin-x64/dmg/mu.app &&
+hdiutil create mu-darwin-x64/mu.dmg -srcfolder mu-darwin-x64/dmg &&
+rm -r mu-darwin-x64/dmg
