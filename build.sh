9a7aa5be9d8137be0d2ff5f269a7ea974b4b3e2b
Diego Vilariño <diego.vilarino@gmail.com>
2017-10-17 01:09:18 +0200

    use appdmg to build dmg

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 # Requirements:
+#
 # npm install -g electron-packager
+# npm install -g appdmg
 #
 # sh build.sh
 #
 electron-packager ./ --platform=darwin --arch=x64 --overwrite --version=0.0.1 --icon=mu.icns --out=bin &&
-mkdir bin/mu-darwin-x64/dmg &&
-ln -s /Applications bin/mu-darwin-x64/dmg/Applications &&
-mv bin/mu-darwin-x64/mu.app bin/mu-darwin-x64/dmg/mu.app &&
-hdiutil create bin/mu-darwin-x64/mu.dmg -srcfolder bin/mu-darwin-x64/dmg &&
-rm -r bin/mu-darwin-x64/dmg
+appdmg appdmg.json bin/mu-darwin-x64/mu.dmg &&
+rm -r bin/mu-darwin-x64/mu.app
